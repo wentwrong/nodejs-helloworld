2a13c2301eef1852dbc089cb40b544d3b0483b17
Pavel Redrukhin <wabloxa@yandex.ru>
2020-02-05 19:02:22 +0300

    Add assert for response code

--- src/my-server.js
+++ src/my-server.js
@@ -1,22 +1,26 @@
-const util = require('util');
+const { DEFAULT_HOST, DEFAULT_PORT } = require('./constants');
 
 class Server {
-    constructor(app, host = '127.0.0.1', port = 3000) {
+    constructor (app, host = DEFAULT_HOST, port = DEFAULT_PORT) {
         this.app = app;
         this.port = port;
         this.host = host;
     }
 
-    start() {
-        return new Promise((resolve, reject) => { 
-            this.server = this.app.listen(this.port, resolve).on('error', reject)
-        });
+    start () {
+        return new Promise((resolve, reject) => {
+            this.server = this.app.listen(this.port, resolve).on('error', reject);
+        })
+            .then(() => console.log(`Server started on ${this.host}:${this.port}`))
+            .catch(() => console.log(`Server failed to start`));
     }
 
-    stop() {
-        return new Promise((resolve, reject) => { 
-            this.server = this.server.close(resolve).on('error', reject)
-        });
+    stop () {
+        return new Promise((resolve, reject) => {
+            this.server = this.server.close(resolve).on('error', reject);
+        })
+            .then(() => console.log(`Server stop listening for connections`))
+            .catch(() => console.log(`Failed to close server`));
     }
 }
 
