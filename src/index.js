980d6c344b678b5088e2d89bd6dc0b5a463d5900
Pavel Redrukhin <wabloxa@yandex.ru>
2020-02-06 10:23:32 +0300

    Small improvements: static dir and more clear code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const path = require('path');
 const express = require('express');
 const Server = require('./my-server');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
-});
+app.use(express.static('static'));
+
+if (require.main === module)
+    (async () => await new Server(app).start())();
+
 
 module.exports = {
     server: new Server(app)
