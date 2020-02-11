e6fa41f6f1741ba6b07eda34648e9c96c1854d62
Pavel Redrukhin <wabloxa@yandex.ru>
2020-02-11 11:22:19 +0300

    Revert to incapsulating express inside App

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const Server = require('./my-server');
 const { DEFAULT_PORT, DEFAULT_HOST } = require('./config');
+const router = require('./router');
 
 class App {
     constructor ({ port = DEFAULT_PORT, host = DEFAULT_HOST } = {}) {
-        this.express = express();
-        this.express.use(express.static('static'));
+        this.server = new Server(this._configExpress(express()), port, host);
+    }
 
-        this.server = new Server(this.express, port, host);
+    _configExpress (e) {
+        e.set('view engine', 'pug');
+        e.use(router);
+        return e;
     }
 
     async run () {
