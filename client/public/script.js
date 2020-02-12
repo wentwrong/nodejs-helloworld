c60701eb199f344e3a89f6a994af71e9055e3c79
Pavel Redrukhin <wabloxa@yandex.ru>
2020-02-12 16:19:14 +0300

    Client-side app class

--- client/public/script.js
+++ client/public/script.js
@@ -1,25 +1,33 @@
-document.addEventListener('DOMContentLoaded', renderPage);
-
-const pullRequestsDiv = document.querySelector('.pull-requests');
-
-async function renderPage () {
-    try {
-        const response = await fetch('api/v1/pulls/');
-        const { pullRequestList } = await response.json();
-
-        pullRequestList.forEach(pr => createNewPullRequestDiv(pr));
+class App {
+    constructor() {
+        this.pullRequestsDiv = document.querySelector('.pull-requests');
+        this.renderPage();
     }
-    catch (err) {
-        const errorMessage = document.createElement('div');
-        errorMessage.innerHTML = 'An error has occured while fetching data from server.';
 
-        pullRequestsDiv.appendChild(errorMessage);
+    async renderPage () {
+        try {
+            const response = await fetch('api/v1/pulls/');
+            const { pullRequestList } = await response.json();
+    
+            pullRequestList.forEach(pr => this._createNewPullRequestDiv(pr));
+        }
+        catch (err) {
+            console.log(err);
+            const errorMessage = document.createElement('div');
+            errorMessage.innerHTML = 'An error has occured while fetching data from server.';
+    
+            this.pullRequestsDiv.appendChild(errorMessage);
+        }
     }
-}
 
-function createNewPullRequestDiv (pullRequest) {
-    const pullRequestDiv = document.createElement('div');
-
-    pullRequestDiv.innerHTML = `<p><a href="${pullRequest['html_url']}">${pullRequest['title']}</a> by ${pullRequest['user']['login']}</p>`;
-    pullRequestsDiv.appendChild(pullRequestDiv);
+    _createNewPullRequestDiv (pullRequest) {
+        const pullRequestDiv = document.createElement('div');
+    
+        pullRequestDiv.innerHTML = (`<p>
+                                    <a href="${pullRequest['html_url']}">
+                                    ${pullRequest['title']}
+                                    </a> by ${pullRequest['user']['login']}</p>`);
+       
+        this.pullRequestsDiv.appendChild(pullRequestDiv);
+    }    
 }
