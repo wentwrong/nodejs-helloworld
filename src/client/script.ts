2cb9fb09489404c207ed582c4a117ba54e94667f
Pavel Redrukhin <wabloxa@yandex.ru>
2020-02-27 11:49:44 +0300

    Add materialize css
    * Better visual representation
    * Moment.js for data formating

--- src/client/script.ts
+++ src/client/script.ts
@@ -1,12 +1,26 @@
 import { PullRequestList } from '../shared/interfaces/pullRequests';
-import { createPullsPage } from './templates';
+import { createPullsPage, createLoader } from './templates';
 import pullsController from './controllers/pullsController';
 import errorRegister from '../shared/errorRegister';
 import debugFactory from '../shared/debugFactory';
+import { reloadBtnClick } from './eventhandler';
+import { ClientApp } from './interfaces/clientApp';
 
 const debug = debugFactory('app');
 
-export default class App {
+export default class App implements ClientApp {
+    async init (): Promise<void> {
+        try {
+            this.setRoot(createLoader());
+            this.setEventHandlers();
+        }
+        catch (err) {
+            debug.error(err);
+
+            await errorRegister(err.stack);
+        }
+    }
+
     async render (): Promise<void> {
         try {
             const pulls: PullRequestList = await pullsController.list();
@@ -20,12 +34,21 @@ export default class App {
         }
     }
 
-    private setRoot (html: string): void {
+    setRoot (html: string): void {
         const output = document?.getElementById('root');
 
         if (output)
             output.innerHTML = html;
         else
             throw new Error('Div with id = root was not found');
+    }
+
+    private setEventHandlers (): void {
+        const reloadButton = document?.getElementById('reload-btn');
+
+        if (reloadButton)
+            reloadButton.addEventListener('click', () => reloadBtnClick(this));
+        else
+            throw new Error('Button with id = reload-btn was not found');
     }
 }
