6cea24dd181545738a618882e7caa9377089363b
Pavel Redrukhin <wabloxa@yandex.ru>
2020-03-05 19:11:18 +0300

    Fix of invalid module name

--- src/mock-github/controllers/mockUsersController.ts
+++ src/mock-github/controllers/mockUsersController.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { User } from '../../shared/interfaces/user';
-import usersModel from '../models/UsersModel';
+import usersModel from '../models/usersModel';
 
 export default class MockUsersController {
     static async infoByUsername (req: express.Request, res: express.Response): Promise<void> {
